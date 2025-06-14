5f22b4f98f3898fc5703cca87041ca480a6a45dd
NAVYA MARAM <navyamaram0227@gmail.com>
2025-06-14 19:34:14 +0530

    Add files via upload

---
+++ Insertion_sort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+public class Insertion_sort {
+    public static void main(String[] args){
+        int[] arr={2,9,5,4,3,1};
+        insertionsort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void insertionsort(int[] nums){
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j>0;j--){
+                if(nums[j]<nums[j-1]){
+                    int temp=nums[j];
+                    nums[j]=nums[j-1];
+                    nums[j-1]=temp;
+                }else{
+                    break;
+                }
+
+            }
+        }
+    }
+    
+}
